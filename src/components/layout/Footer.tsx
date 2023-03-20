8e73c6b54e78df7f1de09aa89fd99757a70dc7db
khasmir04 <kcaluscusao@gmail.com>
2023-03-20 17:39:33 +0800

    Changed footer background color

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -4,7 +4,7 @@ import NavButton from '../common/NavButton'
 
 const Footer = () => {
   return (
-    <div className='w-100 h-[80px] p-5 bg-[#6320ee]'>
+    <div className='w-100 h-[80px] p-5 bg-[#1b1919]'>
       <div className='container m-auto flex justify-between items-center'>
         <p className='text-3xl ml-auto'>{`< Dev KJ />`}</p>
       </div>
