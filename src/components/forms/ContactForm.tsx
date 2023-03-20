4092285d211e1c25af0a15d62738c74474c1de96
khasmir04 <kcaluscusao@gmail.com>
2023-03-20 20:22:32 +0800

    Fixed styling issues with contact form

--- src/components/forms/ContactForm.tsx
+++ src/components/forms/ContactForm.tsx
@@ -21,7 +21,7 @@ const ContactForm = () => {
     <form onSubmit={handleSubmit} className='p-9 bg-[#1b1919] text-white flex flex-col rounded-xl space-y-8'>
       <h2 className='text-3xl'>Let's get connected</h2>
       <div>
-        <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 text-gray-300">Your email</label>
+        <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-300">Your email</label>
         <input type="email" id="email" name='email' className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-primary-500 focus:border-primary-500 shadow-sm-light" placeholder="name@khasmir.com" required />
         <ValidationError
           prefix="Email"
@@ -30,7 +30,7 @@ const ContactForm = () => {
         />
       </div>
       <div className="sm:col-span-2">
-        <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 text-gray-300">Your message</label>
+        <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-300">Your message</label>
         <textarea id="message" name='message' rows={6} className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-primary-500 focus:border-primary-500" placeholder="Leave a comment..." defaultValue={""} />
         <ValidationError
           prefix="Message"
