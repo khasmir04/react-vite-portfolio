35bbec61ef72357fe1cedd7a4fb2cb4626ae9578
khasmir04 <kcaluscusao@gmail.com>
2023-07-31 16:36:29 +0800

    Add framer motion and update design

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,8 +5,24 @@ module.exports = {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        default_morph: {
+          "0%": { "border-radius": "60% 40% 30% 70%/60% 30% 70% 40%" },
+          "50%": { "border-radius": "30% 60% 70% 40%/50% 60% 30% 60%" },
+          "100%": { "border-radius": "60% 40% 30% 70%/60% 30% 70% 40%" },
+        },
+      },
+      animation: {
+        image_morph: "default_morph 8s ease-in-out infinite",
+      },
+      fontFamily: {
+        // Use other fonts
+        // poppins: "Poppins",
+      },
+    },
   },
   plugins: [],
 }
